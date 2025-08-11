f4adbfae6335e3df1b3178ee7766dc5401933974
Lucas Oliveira <lima1301lucas@gmail.com>
2025-08-11 19:06:00 -0300

    Configuração de linguagem com i18next e context

---
+++ src/i18n.js
@@ -0,0 +1,29 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import translationEN from './translations/en.json';
+import translationPT from './translations/pt.json';
+
+const resources = {
+  en: { translation: translationEN },
+  pt: { translation: translationPT },
+};
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'pt',
+    debug: false,
+    interpolation: {
+      escapeValue: false,
+    },
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage'],
+    },
+  });
+
+export default i18n;
