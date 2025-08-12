76629c87aab8cd58dd5b6502584139171001bc69
Lucas Oliveira <lima1301lucas@gmail.com>
2025-08-12 20:48:21 -0300

    Implementação do componete de loading e reajuste nas variáveis de cor

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Header from './components/Header/Header'
+import Loading from './components/Loading/Loading'
 
 function App() {
   return (
     <>
-      <Header />
+      <Loading />
     </>
   );
 }
