82fd0f8493395470491ddc61478ddf846574e2f9
Lucas Oliveira <lima1301lucas@gmail.com>
2025-08-12 21:27:19 -0300

    Início da implementação do componete de menu

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,30 @@
+import Header from '../Header/Header';
+import './Menu.scss';
+
+function Menu() {
+  return (
+    <section>
+      <nav className="menu">
+        <Header />
+
+        <ul className="menu-nav">
+          <li><span>HOME</span></li>
+          <li><span>SOBRE MIM</span></li>
+          <li><span>EXPERIÊNCIAS</span></li>
+          <li><span>HABILIDADES</span></li>
+          <li><span>PROJETOS</span></li>
+          <li><span>CONTATO</span></li>
+        </ul>
+
+        <div className="menu-social">
+          <a className="icon linkedin" href="https://www.linkedin.com/in/lucas-lima-1301/" target="_blank" rel="noopener noreferrer"></a>
+          <a className="icon github" href="https://github.com/lima1301lucas" target="_blank" rel="noopener noreferrer"></a>
+          <a className="icon behance" href="https://www.behance.net/lucas-lima-1301" target="_blank" rel="noopener noreferrer"></a>
+          <a className="icon devto" href="https://dev.to/lima1301lucas" target="_blank" rel="noopener noreferrer"></a>
+        </div>
+      </nav>
+    </section>
+  );
+};
+
+export default Menu;
